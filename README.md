f8c0e7525be6c1f73bbeead7ee8024551a2b149e
Tipu1996 <tipu.solehria.com>
2023-11-05 02:07:00 +0200

    minor commented code added in main.go for possible future use

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GoLang_Todo_API
+This is a Todo REST API built on GoLang using Gin and MongoDB
--- main.go
+++ main.go
@@ -3,44 +3,77 @@ package main
 import (
 	"context"
 	"example/libraryAPI/routes"
-	"fmt"
 	"log"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
-
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+// func connectToMongoDB() *mongo.Client {
+// 	if err := godotenv.Load(); err != nil {
+// 		log.Println("No .env file found")
+// 	}
+// 	uri := os.Getenv("MONGODB_URI")
+// 	if uri == "" {
+// 		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+// 	}
+
+// 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	// Ping the MongoDB server to confirm the connection
+// 	err = client.Ping(context.TODO(), nil)
+// 	if err != nil {
+// 		log.Fatal("Failed to ping MongoDB server")
+// 	}
+
+// 	// Log a message confirming the successful connection
+// 	log.Println("Successfully connected to MongoDB server!")
+
+// 	defer func() {
+// 		if err := client.Disconnect(context.TODO()); err != nil {
+// 			panic(err)
+// 		}
+// 	}()
+
+// 	return client
+// }
+
 func main() {
+	// client := connectToMongoDB()
 	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %v", err)
+		log.Println("No .env file found")
 	}
 
-	// Now you can access the environment variables as you normally would
-	mongodb_uri := os.Getenv("MONGODB_URI")
-
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-
-	opts := options.Client().ApplyURI(mongodb_uri).SetServerAPIOptions(serverAPI)
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), opts)
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
+
+	// Ping the MongoDB server to confirm the connection
+	err = client.Ping(context.TODO(), nil)
+	if err != nil {
+		log.Fatal("Failed to ping MongoDB server")
+	}
+
+	// Log a message confirming the successful connection
+	log.Println("Successfully connected to MongoDB server!")
+
 	defer func() {
-		if err = client.Disconnect(context.TODO()); err != nil {
+		if err := client.Disconnect(context.TODO()); err != nil {
 			panic(err)
 		}
 	}()
-	// Send a ping to confirm a successful connection
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{Key: "ping", Value: 1}}).Err(); err != nil {
-		panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
 	router := gin.Default()
 	routes.SetupRoutes(router, client)
 	router.Run("localhost:9090")
